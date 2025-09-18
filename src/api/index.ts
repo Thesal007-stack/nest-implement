f0693f890049f79e82eac023037646d7715f7fc3
visal.torn@wingbank.com.kh <visal.torn@wingbank.com.kh>
2025-09-18 15:03:45 +0700

    ok

---
+++ src/api/index.ts
@@ -0,0 +1,16 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from '../app.module';
+import * as express from 'express';
+import { ExpressAdapter } from '@nestjs/platform-express';
+import { RequestHandler } from 'express';
+
+const server = express();
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule, new ExpressAdapter(server));
+  await app.init();
+}
+
+bootstrap();
+
+export default server as RequestHandler;
